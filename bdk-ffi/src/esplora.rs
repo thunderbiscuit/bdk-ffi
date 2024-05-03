6ce1941db0263558aa4719da393c42a42070b31d
thunderbiscuit <thunderbiscuit@protonmail.com>
2024-05-03 15:34:08 -0400

    temp1

--- bdk-ffi/src/esplora.rs
+++ bdk-ffi/src/esplora.rs
@@ -1,13 +1,16 @@
+use std::ops::{Deref};
 use crate::error::EsploraError;
-use crate::wallet::{Update, Wallet};
-
+use crate::wallet::{Update};
 use crate::bitcoin::Transaction;
+use crate::types::FullScanRequest;
+
 use bdk::bitcoin::Transaction as BdkTransaction;
-use bdk::wallet::Update as BdkUpdate;
+use bdk::chain::spk_client::FullScanResult as BdkFullScanResult;
 use bdk_esplora::esplora_client::{BlockingClient, Builder};
 use bdk_esplora::EsploraExt;
 
 use std::sync::Arc;
+use bdk::KeychainKind;
 
 pub struct EsploraClient(BlockingClient);
 
@@ -17,34 +20,52 @@ impl EsploraClient {
         Self(client)
     }
 
-    // This is a temporary solution for scanning. The long-term solution involves not passing
-    // the wallet to the client at all.
     pub fn full_scan(
         &self,
-        wallet: Arc<Wallet>,
+        request: Arc<FullScanRequest>,
         stop_gap: u64,
         parallel_requests: u64,
     ) -> Result<Arc<Update>, EsploraError> {
-        let wallet = wallet.get_wallet();
 
-        let previous_tip = wallet.latest_checkpoint();
-        let keychain_spks = wallet.all_unbounded_spk_iters().into_iter().collect();
+        // let previous_tip = wallet.latest_checkpoint();
+        // let keychain_spks = wallet.all_unbounded_spk_iters().into_iter().collect();
 
-        let (update_graph, last_active_indices) = self
-            .0
-            .full_scan(keychain_spks, stop_gap as usize, parallel_requests as usize)
-            .map_err(|e| EsploraError::from(*e))?;
+        // let (update_graph, last_active_indices) = self
+        //     .0
+        //     .full_scan(keychain_spks, stop_gap as usize, parallel_requests as usize)
+        //     .map_err(|e| EsploraError::from(*e))?;
+        //
+        // let missing_heights = update_graph.missing_heights(wallet.local_chain());
+        // let chain_update = self
+        //     .0
+        //     .update_local_chain(previous_tip, missing_heights)
+        //     .map_err(|e| EsploraError::from(*e))?;
+        //
+        // let update = BdkUpdate {
+        //     last_active_indices,
+        //     graph: update_graph,
+        //     chain: Some(chain_update),
+        // };
 
-        let missing_heights = update_graph.missing_heights(wallet.local_chain());
-        let chain_update = self
-            .0
-            .update_local_chain(previous_tip, missing_heights)
-            .map_err(|e| EsploraError::from(*e))?;
+        // let update = self.0.full_scan(
+        //     request.0.clone(),
+        //     stop_gap as usize,
+        //     parallel_requests as usize,
+        // );
 
-        let update = BdkUpdate {
-            last_active_indices,
-            graph: update_graph,
-            chain: Some(chain_update),
+        let result: BdkFullScanResult<KeychainKind> = self.0.full_scan(
+            // throw appropriate error if request is empty
+            // request.0.take().expect("Request is empty"),
+            request.0.lock().unwrap().deref(),
+            // request.0.lock().unwrap().deref().take(),
+            stop_gap as usize,
+            parallel_requests as usize,
+        )?;
+
+        let update = bdk::wallet::Update {
+            last_active_indices: result.last_active_indices,
+            graph: result.graph_update,
+            chain: Some(result.chain_update),
         };
 
         Ok(Arc::new(Update(update)))
